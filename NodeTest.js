f8aca38bac8340948aae097bd19344232e14e0a1
Nadin <Nadin@192.168.1.8>
2017-06-20 19:56:16 +0200

    Aufgabe 10

--- NodeTest.js
+++ NodeTest.js
@@ -1,2 +1,19 @@
-console.log("Hallo");
+"use strict";
+const http = require("http");
+const Url = require("url");
+let server = http.createServer();
+server.addListener("request", handleRequest);
+server.listen(process.env.port || 8100);
+function handleRequest(_request, _response) {
+    console.log(_request.url);
+    _response.setHeader("Access-Control-Allow-Origin", "*");
+    _response.setHeader("content-type", "text/html; charset=utf-8");
+    let query = Url.parse(_request.url, true).query;
+    console.log(query);
+    let key;
+    for (key in query)
+        console.log(key + ":" + query[key]);
+    _response.write("Die Daten sind angekommen!");
+    _response.end();
+}
 //# sourceMappingURL=NodeTest.js.map
--- NodeTest.ts
+++ NodeTest.ts
@@ -1,1 +1,27 @@
-console.log("Hallo");
+
+import http = require("http");
+import Url = require("url");
+
+interface Parameters {
+    [key: string]: string;
+    }
+
+let server: http.Server = http.createServer();
+server.addListener("request", handleRequest);
+server.listen(process.env.port || 8100);
+
+
+function handleRequest(_request: http.IncomingMessage, _response: http.ServerResponse): void {
+    
+    console.log(_request.url);
+    _response.setHeader("Access-Control-Allow-Origin", "*");
+    _response.setHeader("content-type", "text/html; charset=utf-8");
+    let query: Parameters = Url.parse(_request.url, true).query;
+    console.log(query);
+    let key: string;
+    for (key in query)
+        console.log(key + ":" + query[key]);    
+    
+    _response.write("Die Daten sind angekommen!");
+    _response.end();
+}
