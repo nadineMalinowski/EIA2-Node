b691d5ab69fe3a8cf48d9af76f7814dcadf5f7a3
Nadin <Nadin@PC-Nadine.hfu-open.fu.hs-furtwangen.de>
2017-06-22 12:52:06 +0200

    Aufgabe 10

--- NodeTest.js
+++ NodeTest.js
@@ -2,8 +2,12 @@
 const http = require("http");
 const Url = require("url");
 let server = http.createServer();
+server.addListener("listening", onListen);
 server.addListener("request", handleRequest);
 server.listen(process.env.port || 8100);
+function onListen() {
+    console.log("Hello Im your server today");
+}
 function handleRequest(_request, _response) {
     console.log(_request.url);
     _response.setHeader("Access-Control-Allow-Origin", "*");
@@ -13,6 +17,21 @@ function handleRequest(_request, _response) {
     let key;
     for (key in query)
         console.log(key + ":" + query[key]);
+    _response.write("Sie haben folgende Eissorten ausgewaehlt: <br>");
+    _response.write("Erdbeere: " + query["Erdbeere"] + "<br>");
+    _response.write("Zitrone: " + query["Zitrone"] + "<br>");
+    _response.write("Vanille: " + query["Vanille"] + "<br>");
+    _response.write("Schokolade: " + query["Schokolade"] + "<br>");
+    _response.write("Nuss: " + query["Nuss"] + "<br>");
+    _response.write("Melone: " + query["Melone"] + "<br>");
+    _response.write("Joghurt: " + query["Joghurt"] + "<br>");
+    _response.write("Banane: " + query["Banane"] + "<br>");
+    _response.write("Kuerbis: " + query["Kuerbis"] + "<br>");
+    _response.write("Minze: " + query["Minze"] + "<br>");
+    _response.write("Cookie: " + query["Cookie"] + "<br>");
+    _response.write("<br>");
+    _response.write("Folgende Darreichungsform wurde ausgewaehlt: " + query["Darreichungsform"] + "<br>");
+    _response.write("Folgende Zusaetze wurden ausgewaehlt: " + query["Zusaetze"] + "<br>");
     _response.write("Die Daten sind angekommen!");
     _response.end();
 }
--- NodeTest.ts
+++ NodeTest.ts
@@ -7,12 +7,15 @@ interface Parameters {
     }
 
 let server: http.Server = http.createServer();
-server.addListener("request", handleRequest);
-server.listen(process.env.port || 8100);
+    server.addListener("listening", onListen);
+    server.addListener("request", handleRequest);
+    server.listen(process.env.port || 8100);
 
+function onListen(): void {
+    console.log ("Hello Im your server today");
+    }
 
 function handleRequest(_request: http.IncomingMessage, _response: http.ServerResponse): void {
-    
     console.log(_request.url);
     _response.setHeader("Access-Control-Allow-Origin", "*");
     _response.setHeader("content-type", "text/html; charset=utf-8");
@@ -20,8 +23,23 @@ function handleRequest(_request: http.IncomingMessage, _response: http.ServerRes
     console.log(query);
     let key: string;
     for (key in query)
-        console.log(key + ":" + query[key]);    
+        console.log(key + ":" + query[key]);
     
+    _response.write("Sie haben folgende Eissorten ausgewaehlt: <br>");
+    _response.write("Erdbeere: " + query["Erdbeere"] + "<br>");
+    _response.write("Zitrone: " + query["Zitrone"] + "<br>");
+    _response.write("Vanille: " + query["Vanille"] + "<br>");
+    _response.write("Schokolade: " + query["Schokolade"] + "<br>");
+    _response.write("Nuss: " + query["Nuss"] + "<br>");
+    _response.write("Melone: " + query["Melone"] + "<br>");
+    _response.write("Joghurt: " + query["Joghurt"] + "<br>");
+    _response.write("Banane: " + query["Banane"] + "<br>");
+    _response.write("Kuerbis: " + query["Kuerbis"] + "<br>");
+    _response.write("Minze: " + query["Minze"] + "<br>");
+    _response.write("Cookie: " + query["Cookie"] + "<br>");
+    _response.write("<br>");
+    _response.write("Folgende Darreichungsform wurde ausgewaehlt: " + query["Darreichungsform"] + "<br>");
+    _response.write("Folgende Zusaetze wurden ausgewaehlt: " + query["Zusaetze"] + "<br>");
     _response.write("Die Daten sind angekommen!");
     _response.end();
 }
